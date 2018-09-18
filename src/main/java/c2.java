50d3687d5138de13a4c6ebc4ba98c21ab4785c7d
alecabreralituma@gmail.com <alecabreralituma@gmail.com>
2018-09-18 10:43:36 -0500

    initial

---
+++ src/main/java/c2.java
@@ -0,0 +1,16 @@
+public class c2 {
+
+    public void m2()
+    {
+        int r;
+        int l;
+        int x;
+        x=2;
+        l=1;
+        r=x+l;
+        x=r+l;
+        l=x-1;
+
+
+    }
+}
